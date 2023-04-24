d78c60883366eac5446cc29f4a90fac9130869c6
Тимофей Синюшкин <y.timko56@gmail.com>
2023-04-24 23:49:21 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.css
    	deleted:    screenshots/error.png
    	deleted:    screenshots/refer_body.png
    	deleted:    screenshots/refer_header.png
    	new file:   src/gecko_url/1.png
    	new file:   src/gecko_url/10.png
    	new file:   src/gecko_url/11.png
    	new file:   src/gecko_url/12.png
    	new file:   src/gecko_url/13.png
    	new file:   src/gecko_url/14.png
    	new file:   src/gecko_url/15.png
    	new file:   src/gecko_url/16.png
    	new file:   src/gecko_url/17.png
    	new file:   src/gecko_url/18.png
    	new file:   src/gecko_url/19.png
    	new file:   src/gecko_url/2.png
    	new file:   src/gecko_url/20.png
    	new file:   src/gecko_url/21.png
    	new file:   src/gecko_url/22.png
    	new file:   src/gecko_url/3.png
    	new file:   src/gecko_url/4.png
    	new file:   src/gecko_url/5.png
    	new file:   src/gecko_url/6.png
    	new file:   src/gecko_url/7.png
    	new file:   src/gecko_url/8.png
    	new file:   src/gecko_url/9.png
    	new file:   src/images/undo.png
    	modified:   src/index.js
    	modified:   src/page_elements/geckoList.jsx
    	modified:   src/page_elements/geckoList.module.css
    	modified:   src/wrapper/wrapper.jsx
    	modified:   src/wrapper/wrapper.module.css

--- src/page_elements/geckoList.jsx
+++ src/page_elements/geckoList.jsx
@@ -1,37 +1,153 @@
 import React, { useEffect, useState } from "react";
 import classes from "./geckoList.module.css";
+import wrapperClasses from "../wrapper/wrapper.module.css";
 
-function GeckoList() {
+function GeckoList({ basketCallback, basket_state }) {
 
-    // 
+    const url = "http://217.71.129.139:5308" //http://217.71.129.139:5308/  http://localhost:2000/
+
+    const [data, setData] = useState();
+
+    const [curTasks, setCurTasks] = useState(1);
 
     useEffect(() => {
         const fetchData = () => {
-            fetch('http://localhost:2000/', { //http://217.71.129.139:5308/
+            let options = {
                 method: 'GET',
-                mode: 'no-cors',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                // body: JSON.stringify()
-            })
+                mode: 'cors',
+            }
+            fetch(url, options)
                 .then(response => response.json())
-                .then(data => console.log(data))
+                .then(data => setData(data))
                 .catch(error => console.log('Ошибка запроса', error));
         }
-        fetchData();
+        function scrollEvt() {
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.documentElement.scrollHeight;
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (documentHeight - (scrollTop + windowHeight) < 500) {
+                console.log("конец");
+                if (data && ((curTasks * 8) > data.length))
+                    return;
 
-    }, []);
+                setCurTasks(curTasks + 1);
+            }
+        }
+
+        fetchData();
+        window.addEventListener("scroll", scrollEvt);
+
+        return () => {
+            window.removeEventListener("scroll", scrollEvt);
+        }
+
+    }, [basket_state, curTasks]);
+
+    function outputGeckoList() {
+        if (data) {
+
+            let items_array = data;
+
+            let indx = ((curTasks * 8) >= items_array.length) ? (items_array.length) : (curTasks * 8);
+            items_array = items_array.slice(0, indx);
+            console.log(curTasks);
+
+            return (
+                <div>
+                    {items_array.map((item) => (
+                        <GeckoItem id={item.id} count={item.count} desc={item.description} price={item.price} url={item.url} name={item.name} basketCallback={basketCallback} basketState={basket_state}>
+                        </GeckoItem>
+                    ))}
+                </div>
+            )
+        }
+        else {
+            return (
+                <EmptyItem>
+                </EmptyItem>
+            )
+        }
+    }
 
     return (
         <div className={classes.geckoList}>
-            {/* {data && data.map((row) => (
-                <div>
-                    dsfasfsadfasd
-                </div>
-            ))} */}
+
+            {outputGeckoList()}
+
         </div>
     );
 }
 
 export default GeckoList;
+
+
+function GeckoItem({ id, count, desc, price, url, name, basketCallback, basketState }) {
+
+    const [curCount, setCount] = useState(0);
+
+    const [buyButtonClassName, setBuyButtonClassName] = useState(classes.addToBasket + " " + classes.buttonStyle);
+
+    const [fullDescription, setFullDescrition] = useState(false);
+
+    useEffect(() => {
+        if (!basketState[id])
+            setCount(0);
+
+        if (!count)
+            setBuyButtonClassName(buyButtonClassName + " " + classes.disableButton);
+    }, [basketState, count, fullDescription]);
+
+    function addToBasket() {
+        if (curCount == count)
+            return;
+        basketCallback(id, curCount + 1, price, name);
+        setCount(curCount + 1);
+    }
+
+    function removeFromBasket() {
+        if (!curCount)
+            return;
+        basketCallback(id, curCount - 1, price, name);
+        setCount(curCount - 1);
+    }
+
+
+    return (
+        <div className={classes.geckoItem}>
+            <div className={classes.imageBlock}>
+                <img src={require("../gecko_url/" + url + ".png")} width="300px" />
+            </div>
+            <div className={classes.textBlock}>
+                <span className={classes.itemName}><b>{name}</b></span>
+                <span className={classes.itemPrice}>Цена: {price}₽</span>
+                <span className={classes.itemCount}>{count ? `В наличии: ${count}шт.`: "Нет в наличии"}</span>
+                <div className={classes.itemDescription}>
+                    <span className={classes.itemDesc + " " + (fullDescription ? classes.fullDescription : "")}>&emsp;{desc}</span>
+                    <button className={classes.fullDescriptionButton + " " + classes.buttonStyle} onClick={() => setFullDescrition(!fullDescription)}>
+                        {fullDescription ? "Свернуть" : "Развернуть"}
+                    </button>
+                </div>
+                <div className={classes.buttonRow}>
+                    <div className={classes.buttonBlock}>
+                        <button className={classes.minusButton + " " + classes.addremoveButtons + " " + classes.buttonStyle} onClick={removeFromBasket}>&#45;</button>
+                        <span className={classes.countNumber}>{curCount}</span>
+                        <button className={classes.plusButton + " " + classes.addremoveButtons + " " + classes.buttonStyle} onClick={addToBasket}>&#43;</button>
+                        <div className={buyButtonClassName}>
+                            <span>Добавить в корзину</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+function EmptyItem() {
+    return (
+        <div className={classes.emptyItem}>
+            <span>
+                нет соединения с сервером
+            </span>
+        </div>
+    );
+}
--- src/wrapper/wrapper.jsx
+++ src/wrapper/wrapper.jsx
@@ -1,8 +1,257 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./wrapper.module.css";
 import GeckoList from "../page_elements/geckoList";
+import { CSSTransition } from "react-transition-group";
 
 function Wrapper() {
+
+    const [basketState, setBasketState] = useState({});
+
+    const [isShow, setShowMessage] = useState(false);
+    const [isShowError, setShowError] = useState(false);
+
+    const [CardNumber, setCardNumber] = useState("");
+    const [Address, setAddress] = useState("");
+    const [Email, setEmail] = useState("");
+
+    const [{ emptyBasketError, notConfirmedInputs }, setErrorState] = useState({ emptyBasketError: false, notConfirmedInputs: false });
+
+    useEffect(() => {
+
+        if (isShow || isShowError)
+            setTimeout(() => {
+                setShowMessage(false);
+                setShowError(false);
+            }, 5000);
+
+
+        if (Address && Address !== -1)
+            document.querySelector("." + classes.addressInput).style.border = "2px solid green";
+        else if (Address === -1)
+            document.querySelector("." + classes.addressInput).style.border = "2px solid red";
+        else
+            document.querySelector("." + classes.addressInput).style.removeProperty("border");
+
+        if (CardNumber && CardNumber !== -1)
+            document.querySelector("." + classes.cardNumberInput).style.border = "2px solid green";
+        else if (CardNumber === -1)
+            document.querySelector("." + classes.cardNumberInput).style.border = "2px solid red";
+        else
+            document.querySelector("." + classes.cardNumberInput).style.removeProperty("border");
+
+        if (Email && Email !== -1)
+            document.querySelector("." + classes.emailInput).style.border = "2px solid green";
+        else if (Email === -1)
+            document.querySelector("." + classes.emailInput).style.border = "2px solid red";
+        else
+            document.querySelector("." + classes.emailInput).style.removeProperty("border");
+
+    }, [Address, Email, CardNumber, isShow, isShowError]);
+
+    const updateBasket = (id, count, price, name) => {
+        if (count) {
+            setBasketState(prevState => ({ ...prevState, [id]: { count: count, price: price, name: name } }));
+        }
+        else {
+            setBasketState(prevState => {
+                const newData = { ...prevState }
+                delete newData[id]
+                return newData;
+            });
+        }
+    }
+
+    const createBusketList = () => {
+        if (Object.keys(basketState).length !== 0) {
+
+            let items_array = [];
+            for (let key in basketState)
+                items_array.push(basketState[key]);
+            console.log(items_array);
+
+            let total_price = 0;
+
+            for (let key in basketState)
+                total_price += basketState[key].count * basketState[key].price;
+
+            return (
+                <div>
+                    <table className={classes.geckoTable}>
+                        <thead>
+                            <tr>
+                                <th>Имя</th><th>Кол-во</th><th>Сумма</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items_array.map((elem, index) =>
+                                // console.log(elem);
+                                // if (!elem.buyCount) {
+                                <tr className={classes.basketItem}>
+                                    <td>{elem.name}</td>
+                                    <td>{elem.count.toString()}</td>
+                                    <td>{(elem.price * elem.count).toString()}</td>
+                                </tr>
+                                // }
+                            )}
+                        </tbody>
+                    </table>
+                    <div className={classes.totalPrice}>
+                        <span>Общая сумма заказа: </span>
+                        <span className={classes.totalAmount}>{total_price.toString()}</span>
+                    </div>
+                </div>
+
+
+            )
+
+        }
+        else {
+            // console.log(basketState);
+            return (
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <span >Корзина пуста</span>
+                </div>
+            )
+        }
+    };
+
+    const clearList = () => {
+        setBasketState({});
+    };
+
+    const showMessage = () => {
+        setShowMessage(!isShow);
+        setBasketState({});
+    }
+
+    const showError = () => {
+        setShowError(!isShowError);
+        setBasketState({});
+    }
+
+    const createOrder = () => {
+
+        if (Object.keys(basketState).length === 0) {
+            setErrorState({ emptyBasketError: "Корзина пуста!", notConfirmedInputs });
+            return;
+        }
+        else setErrorState({ emptyBasketError: "", notConfirmedInputs: "" });
+
+
+        const is_not_valid = (variable) => (Boolean(variable === -1 || variable === ""))
+
+        if (is_not_valid(CardNumber) || is_not_valid(Address) || is_not_valid(Email)) {
+            setErrorState({ emptyBasketError: false, notConfirmedInputs: "Заполните все поля!" });
+            return;
+        }
+        else setErrorState({ emptyBasketError: false, notConfirmedInputs: false });
+
+        let sendingData = JSON.parse(JSON.stringify(basketState));
+
+        for (let key in sendingData) {
+            sendingData[key].buyCount = sendingData[key].count;
+            delete sendingData[key].count;
+            delete sendingData[key].price;
+            delete sendingData[key].name;
+        }
+
+        fetch("http://217.71.129.139:5308", {
+            method: 'POST',
+            mode: "cors",
+            body: JSON.stringify(sendingData)
+        })
+            .then(result => showMessage())
+            .catch(error => showError());
+    };
+
+    function allowCardNumber() {
+        let input = document.querySelector("." + classes.cardNumberInput);
+        let value = input.value.trim();
+        // checking
+        if ((value.length !== 16) || !(/^[0-9]+$/.test(value))) {
+            setCardNumber(-1);
+            return;
+        }
+
+        input.style.pointerEvents = "none";
+        input.style.color = "#666";
+        setCardNumber(value);
+    };
+
+    function allowAddress() {
+        let input = document.querySelector("." + classes.addressInput);
+        let value = input.value.trim();
+        // checking
+        const reg = /^[а-я\s.]+?\d+/i;
+        if (!reg.test(value)) {
+            setAddress(-1);
+            return;
+        }
+
+        input.style.pointerEvents = "none";
+        input.style.color = "#666";
+        setAddress(value);
+    };
+
+    function allowEmail() {
+        let input = document.querySelector("." + classes.emailInput);
+        let value = input.value.trim();
+
+        const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+        if (!EMAIL_REGEXP.test(value)) {
+            setEmail(-1);
+            return;
+        }
+
+        input.style.pointerEvents = "none";
+        input.style.color = "#666";
+        setEmail(value);
+    }
+
+    function resetCardNumber() {
+        let input = document.querySelector("." + classes.cardNumberInput);
+        input.value = "";
+        input.style.pointerEvents = "auto";
+        input.style.color = "#000"
+
+        setCardNumber("");
+    }
+
+    function resetAddress() {
+        let input = document.querySelector("." + classes.addressInput);
+        input.value = "";
+        input.style.pointerEvents = "auto";
+        input.style.color = "#000"
+
+        setAddress("");
+    }
+
+    function resetEmail() {
+        let input = document.querySelector("." + classes.emailInput);
+        input.value = "";
+        input.style.pointerEvents = "auto";
+        input.style.color = "#000"
+
+        setEmail("");
+    }
+
+    function cardNumberKeys(e) {
+        if (e.key === "Enter") allowCardNumber();
+        if (e.key === "ArrowDown") document.querySelector("." + classes.addressInput).focus();
+    }
+
+    function addressKeys(e) {
+        if (e.key === "Enter") allowAddress();
+        if (e.key === "ArrowDown") document.querySelector("." + classes.emailInput).focus();
+        if (e.key === "ArrowUp") document.querySelector("." + classes.cardNumberInput).focus();
+    }
+
+
+    function emailKeys(e) {
+        if (e.key === "Enter") allowEmail();
+        if (e.key === "ArrowUp") document.querySelector("." + classes.addressInput).focus();
+    }
+
     return (
         <div className={classes.Wrapper}>
             <header className={classes.header + " " + classes.panel}>
@@ -15,22 +264,57 @@ function Wrapper() {
                             <li className={classes.delivery + " " + classes.navigationButtons}>
                                 <img src={require("../images/delivery_icon.png")} height="35px" alt="no" />
                                 <span>Доставка</span>
-                                <div className={classes.dropDown}>
-                                    <span>&emsp;Адреса пунктов самовывоза:<br /></span>
-                                    <ul>
-                                        <li>адрес1</li>
-                                        <li>адрес2</li>
-                                        <li>адрес3</li>
-                                        <li>адрес4</li>
-                                    </ul>
+                                <div className={classes.deliveryDropDown}>
                                     <span>&emsp;Даставку осуществляют только высококлассные профессионалы своего дела. Ими были доставлены сотни животных
                                         со всех точек мира: Африка, Бразилия, Австралия и с доставкой Вашего чешуйчатого малыша в Новосибирске они справятся,
                                         уж поверьте.<br /></span>
+                                    <span>&emsp;Доставим в день заказа, при условии, что он был совершён не позднее 19:00!</span>
                                 </div>
                             </li>
                             <li className={classes.basket + " " + classes.navigationButtons}>
                                 <img src={require("../images/basket_icon.png")} height="35px" alt="no" />
                                 <span>Корзина</span>
+                                <div className={classes.basketDropDown}>
+                                    <span className={classes.basketTitle}>Корзина</span>
+                                    {Boolean(emptyBasketError) && <ErrorMessage>{emptyBasketError}</ErrorMessage>}
+                                    {Boolean(notConfirmedInputs) && <ErrorMessage>{notConfirmedInputs}</ErrorMessage>}
+                                    <div className={classes.basketItems}>
+                                        {createBusketList()}
+                                    </div>
+                                    <div className={classes.userData}>
+                                        <div className={classes.cardNumberBlock + " " + classes.inputBlock}>
+                                            <input className={classes.cardNumberInput + " " + classes.basketInput} type="text" placeholder="Введите номер карты" onKeyDown={(e) => cardNumberKeys(e)} />
+                                            <button className={classes.cardNumberButton + " " + classes.inputButton + " " + classes.basketButton} onClick={allowCardNumber}>
+                                                <span>Подтвердить</span>
+                                            </button>
+                                            <button className={classes.resetButton + " " + classes.basketButton} onClick={resetCardNumber}>
+                                                <img src={require("../images/undo.png")} />
+                                            </button>
+                                        </div>
+                                        <div className={classes.addressBlock + " " + classes.inputBlock}>
+                                            <input className={classes.addressInput + " " + classes.basketInput} type="text" placeholder="Введите адрес доставки" onKeyDown={(e) => addressKeys(e)} />
+                                            <button className={classes.addressButton + " " + classes.inputButton + " " + classes.basketButton} onClick={allowAddress}>
+                                                <span>Подтвердить</span>
+                                            </button>
+                                            <button className={classes.resetButton + " " + classes.basketButton} onClick={resetAddress}>
+                                                <img src={require("../images/undo.png")} />
+                                            </button>
+                                        </div>
+                                        <div className={classes.emailBlock + " " + classes.inputBlock}>
+                                            <input className={classes.emailInput + " " + classes.basketInput} type="email" placeholder="Введите Ваш e-mail" onKeyDown={(e) => emailKeys(e)} />
+                                            <button className={classes.emailButton + " " + classes.inputButton + " " + classes.basketButton} onClick={allowEmail}>
+                                                <span>Подтвердить</span>
+                                            </button>
+                                            <button className={classes.resetButton + " " + classes.basketButton} onClick={resetEmail}>
+                                                <img src={require("../images/undo.png")} />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className={classes.basketButtons}>
+                                        <button onClick={clearList} className={classes.basketButton}><span>Очистить корзину</span></button>
+                                        <button onClick={createOrder} className={classes.basketButton}><span>Совершить заказ</span></button>
+                                    </div>
+                                </div>
                             </li>
                         </ul>
                     </div>
@@ -39,8 +323,41 @@ function Wrapper() {
             </header>
             <main className={classes.main}>
                 <Container>
-                    <GeckoList></GeckoList>
+                    <GeckoList basketCallback={updateBasket} basket_state={basketState}></GeckoList>
                 </Container>
+                <CSSTransition in={isShow} timeout={500} classNames={{
+                    enterActive: classes.successMessageEnterActive,
+                    enterDone: classes.successMessageEnterDone,
+                    exitActive: classes.successMessageExitActive,
+                    exitDone: classes.successMessageExitDone
+                }} unmountOnExit>
+                    <div className={classes.successMessage}>
+                        <span>
+                            Покупка совершена успешно!
+                        </span>
+                        <span>
+                            Ваш новый питомец прибудет в ближайшее время!
+                        </span>
+                        <span>
+                            Ожидайте информации по указанному адресу электронной почты
+                        </span>
+                    </div>
+                </CSSTransition>
+                <CSSTransition in={isShowError} timeout={500} classNames={{
+                    enterActive: classes.successMessageEnterActive,
+                    enterDone: classes.successMessageEnterDone,
+                    exitActive: classes.successMessageExitActive,
+                    exitDone: classes.successMessageExitDone
+                }} unmountOnExit>
+                    <div className={classes.failureMessage}>
+                        <span>
+                            Произошла ошибка при совершении заказа!
+                        </span>
+                        <span>
+                            Попробуйте повторить позже!
+                        </span>
+                    </div>
+                </CSSTransition>
             </main>
             <footer className={classes.footer + " " + classes.panel}>
                 <Container>
@@ -75,6 +392,16 @@ function Container({ children, ...props }) {
     return (
         <div className={classes.container}>
             {children}
+        </div>
+    );
+}
+
+function ErrorMessage({ children }) {
+    return (
+        <div className={classes.errorMessage}>
+            <span>
+                {children}
+            </span>
         </div>
     );
 }
